58a0fa8dfaced019cf7d49e1a733fdc784c0b552
AIFuture180 <aifuture180@gmail.com>
2025-04-08 20:55:48 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add scroll effect for nav
+window.addEventListener('scroll', () => {
+    const nav = document.getElementById('nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
